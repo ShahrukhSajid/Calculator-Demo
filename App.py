876558a5a9223e31cf45a048b00159ffbd74d0b1
ShahrukhSajid <153828251+ShahrukhSajid@users.noreply.github.com>
2024-10-16 23:01:34 -0700

    Update App.py

--- App.py
+++ App.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # Title of the app
-st.title("Simple Calculator")
+st.title("BNU Calculator")
 
 # Input fields for numbers
 num1 = st.number_input("Enter the first number", value=0.0)
